8ffe761f4e6e54df8c91f1770df972e3c2571209
Bulat <sadikovba@gmail.com>
2015-06-12 20:23:36 +0300

    Add checking to press key Enter, v0.13

--- comments-js/app.js
+++ comments-js/app.js
@@ -1,14 +1,29 @@
 var main = function() {
 	"use strict";
+
 	$(".page-main__comments-input-button").on("click", function(event) {
 		var $new_comment = $("<p class=\"page-main__comments-text\">");
 		
-	if ($(".page-main__comments-input").val() !== "") {
-		var $comment_text = $(".page-main__comments-input").val();
-		$new_comment.text($comment_text);
-		$(".page-main__comments-block").append($new_comment);
-		$(".page-main__comments-input").val("");
-	};
+		if ($(".page-main__comments-input").val() !== "") {
+			var $comment_text = $(".page-main__comments-input").val();
+			$new_comment.text($comment_text);
+			$(".page-main__comments-block").append($new_comment);
+			$(".page-main__comments-input").val("");
+		};
+		
+	});
+
+	$(".page-main__comments-input").on("keypress", function(event) {
+		var $new_comment = $("<p class=\"page-main__comments-text\">");
+		
+		if (event.keyCode === 13) {
+			if ($(".page-main__comments-input").val() !== "") {
+				var $comment_text = $(".page-main__comments-input").val();
+				$new_comment.text($comment_text);
+				$(".page-main__comments-block").append($new_comment);
+				$(".page-main__comments-input").val("");
+			};
+		};
 		
 	});
 };
