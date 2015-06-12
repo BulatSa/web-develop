0a80342aa1819c005c2f198f3fdeda229b220149
Bulat <sadikovba@gmail.com>
2015-06-12 20:02:42 +0300

    Add check to empty input, v0.11

--- comments-js/app.js
+++ comments-js/app.js
@@ -2,9 +2,13 @@ var main = function() {
 	"use strict";
 	$(".page-main__comments-input-button").on("click", function(event) {
 		var $new_comment = $("<p class=\"page-main__comments-text\">");
+		
+	if ($(".page-main__comments-input").val() !== "") {
 		var $comment_text = $(".page-main__comments-input").val();
 		$new_comment.text($comment_text);
 		$(".page-main__comments-block").append($new_comment);
+	};
+		
 	});
 };
 
